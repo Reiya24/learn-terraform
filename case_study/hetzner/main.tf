feb2feb7d6133829a61059893c98da262ad5cca6
Reiya24 <reiya2307@gmail.com>
2023-10-18 20:35:04 +0700

    poe: refactor supaya tidak ada force replacement

--- case_study/hetzner/main.tf
+++ case_study/hetzner/main.tf
@@ -23,13 +23,10 @@ resource "hcloud_firewall" "basic_firewall" {
   dynamic "rule" {
     for_each = var.firewall_port
     content {
-      direction = "in"
-      protocol  = "tcp"
-      port      = rule.value
-      source_ips = [
-        "0.0.0.0/0",
-        "::/0"
-      ]
+      direction  = "in"
+      protocol   = "tcp"
+      port       = rule.value
+      source_ips = ["0.0.0.0/0", "::/0"]
     }
   }
 }
@@ -47,13 +44,13 @@ resource "hcloud_network_subnet" "subnet1" {
 }
 
 resource "hcloud_server" "server" {
-  for_each = toset(var.server_list)
+  for_each = { for server in var.server_list : server => server }
 
   name        = each.value
   image       = "ubuntu-22.04"
   location    = "nbg1"
   server_type = "cx11"
-  ssh_keys    = keys(hcloud_ssh_key.ssh_keys)
+  ssh_keys    = [for key in hcloud_ssh_key.ssh_keys : key.id]
 
   labels = {
     type = "terraform"
@@ -61,7 +58,7 @@ resource "hcloud_server" "server" {
 }
 
 resource "hcloud_server_network" "server" {
-  for_each   = toset(var.server_list)
+  for_each   = { for server in var.server_list : server => server }
   server_id  = hcloud_server.server[each.key].id
   network_id = hcloud_network.main_network.id
 }
