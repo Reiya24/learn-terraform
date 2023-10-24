c16f85a96f67f2d6b0012479d53d598217d94dd6
Reiya24 <reiya2307@gmail.com>
2023-10-24 20:53:12 +0700

    cleant

--- fundamental/11modules/main.tf
+++ fundamental/11modules/main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-  source               = "../_modules/ec2"
+  source               = "./_modules/ec2"
   aws_access_key       = var.aws_access_key
   aws_secret_key       = var.aws_secret_key
   list_vm              = var.list_vm
@@ -7,7 +7,7 @@ module "ec2" {
 }
 
 module "sg" {
-  source         = "../_modules/sg"
+  source         = "./_modules/sg"
   aws_access_key = var.aws_access_key
   aws_secret_key = var.aws_secret_key
   vpc_id         = var.vpc_id
